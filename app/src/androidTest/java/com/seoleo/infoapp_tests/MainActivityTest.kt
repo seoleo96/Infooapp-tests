0ccd57e777cd06a1b441cc482496b5a23aa8c8d5
sardoriiiy <leo@indev.expert>
2024-06-21 16:59:55 +0500

    added recycler view

--- app/src/androidTest/java/com/seoleo/infoapp_tests/MainActivityTest.kt
+++ app/src/androidTest/java/com/seoleo/infoapp_tests/MainActivityTest.kt
@@ -1,8 +1,12 @@
 package com.seoleo.infoapp_tests
 
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.hamcrest.Matchers
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,5 +22,21 @@ class MainActivityTest {
     @Test
     fun screen_should_apply_correct_title(){
         toolbarWithTitle(R.string.title).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun contains_recycler_view(){
+        Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withId(R.id.recycler_view),
+                ViewMatchers.withParent(
+                    ViewMatchers.isAssignableFrom(ConstraintLayout::class.java)
+                )
+            )
+        ).check(
+            ViewAssertions.matches(
+                ViewMatchers.isDisplayed()
+            )
+        )
     }
 }
