56d5157a2e92f733cfe6e8ed02c9ed2d193aeda6
sardoriiiy <s.sarimsakov@indev.expert>
2024-06-23 13:00:20 +0500

    text view of activity is displayed test

--- app/src/androidTest/java/com/seoleo/infoapp_tests/MainActivityTest.kt
+++ app/src/androidTest/java/com/seoleo/infoapp_tests/MainActivityTest.kt
@@ -1,5 +1,24 @@
 package com.seoleo.infoapp_tests
 
-import org.junit.Assert.*
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
-class MainActivityTest
+@RunWith(JUnit4::class)
+class MainActivityTest {
+
+    @JvmField
+    @Rule
+    val mainActivityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun text_view_is_displayed() {
+        textViewInsideLinearLayoutInteraction(R.id.text_view).check(
+            ViewAssertions.matches(ViewMatchers.isDisplayed())
+        )
+    }
+}
--- app/src/androidTest/java/com/seoleo/infoapp_tests/extensions.kt
+++ app/src/androidTest/java/com/seoleo/infoapp_tests/extensions.kt
@@ -1,4 +1,16 @@
 package com.seoleo.infoapp_tests
 
-class ext {
+import android.widget.LinearLayout
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.ViewInteraction
+import androidx.test.espresso.matcher.ViewMatchers
+import org.hamcrest.Matchers
+
+fun textViewInsideLinearLayoutInteraction(textView: Int): ViewInteraction {
+    return Espresso.onView(
+        Matchers.allOf(
+            ViewMatchers.withId(textView),
+            ViewMatchers.withParent(ViewMatchers.isAssignableFrom(LinearLayout::class.java))
+        )
+    )
 }
